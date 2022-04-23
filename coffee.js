d872b6eac5f0f7b77029d48678ce1a6c9e079efe
EwuraAnnie <ewuranhyira03@gmail.com>
2022-04-23 16:07:30 +0000

    coffee landing page

---
+++ coffee.js
@@ -0,0 +1,14 @@
+//arguments & parameters
+
+let averageAge;
+let age;
+// let task = () => {
+
+// }
+
+function bless(averageAge) {
+    for( let i= 0; i < averageAge; i++){
+        i++;
+        console.log(i);
+    }
+}
